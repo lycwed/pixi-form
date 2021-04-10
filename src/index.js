0a79e960ef5a47f851d65d0b06a9ed62565c619c
lycwed dev <ylechevalier@gmail.com>
2021-04-10 21:33:56 +0200

    add missing file save

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-var TextInput = require("./text-input-plus");
+var TextInput = require("./pixi-text-input-plus");
 
 Object.assign(PIXI, TextInput);
-
-console.log('PIXI', PIXI);
 
 module.exports = TextInput;
